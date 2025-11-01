f26a939d9f8696aae4fb6f6dee8205686189a06a
AngelOrta <146902560+AngelOrta@users.noreply.github.com>
2025-11-01 15:52:13 -0600

    upd en manejo de errores al registrar y datos que retorna obtenerPorUid

--- src/models/usuarios.models.js
+++ src/models/usuarios.models.js
@@ -1,15 +1,20 @@
 import {pool} from '../db/db.js';
 
 export default class Usuario {
-  constructor(id, uid) {
-    this.uid = uid;
+  constructor(id, rol) {
     this.id = id;
+    this.rol = rol;
   }
 
   static async obtenerPorUid(uid) {
-    const [rows] = await pool.query('SELECT * FROM Usuario WHERE uid_firebase = ?', [uid]);
-    if (!rows.length) return null;
-    return new Usuario(rows[0].id, rows[0].uid_firebase);
+    try{
+      const [rows] = await pool.query('SELECT * FROM Usuario WHERE uid_firebase = ?', [uid]);
+      if (!rows.length) return null;
+      return new Usuario(rows[0].id, rows[0].rol);
+    }catch(error){
+      console.log('Error al obtener Usuario '+ error.message);
+      throw error;
+    }
   }
 
   static async crear(nombre, email, rol, genero, uid, idEmpresa) {
@@ -52,7 +57,7 @@ export default class Usuario {
         await connection.rollback();
 
       console.log(error.message);
-      return null;
+      throw error;
     }finally{
       if (connection)
         connection.release();
